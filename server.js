8239c8a602797a3e3a884359ed548c6536a51849
CedricA <cedric.j.aoun@gmail.com>
2026-03-11 11:08:08 +0100

    add ejs and some placeholders

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-// placeholder
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const ejs = require('ejs');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+app.set('view engine', 'ejs');
+app.use('/styles', express.static('styles'));
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Home Page' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
