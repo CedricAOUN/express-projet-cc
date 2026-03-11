b112b0fd9f0ba5fcd1425132a541daf96d01e419
CedricA <cedric.j.aoun@gmail.com>
2026-03-11 11:29:06 +0100

    add about page + more placeholders

--- server.js
+++ server.js
@@ -7,10 +7,15 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 app.set('view engine', 'ejs');
-app.use('/styles', express.static('styles'));
+app.use("/public", express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('index', { title: 'Home Page' });
+});
+
+app.get('/about', (req, res) => {
+    const whisperCount = 42;
+    res.render('about', { title: 'About Page', whisperCount });
 });
 
 app.listen(PORT, () => {
