5c5aadf41a7427538b7b22ff58cdc6b3545adac5
Charles <c.casabianca@live.fr>
2026-03-11 14:54:16 +0100

    Standard linter ajouté

--- server.js
+++ server.js
@@ -3,39 +3,39 @@ import { getAll, getById, create, updateById, deleteById } from './store.js'
 
 const app = express()
 app.use(express.json())
-app.set('view engine', 'ejs');
-app.use("/public", express.static('public'));
+app.set('view engine', 'ejs')
+app.use('/public', express.static('public'))
 
 app.get('/api/v1/whisper', async (req, res) => {
-    res.json(await getAll())
+  res.json(await getAll())
 })
 
 app.get('/api/v1/whisper/:id', async (req, res) => {
-    const id = parseInt(req.params.id)
-    res.json(await getById(id))
+  const id = parseInt(req.params.id)
+  res.json(await getById(id))
 })
 
 app.post('/api/v1/whisper', async (req, res) => {
-    res.status(201).json(await create(req.body.message))
+  res.status(201).json(await create(req.body.message))
 })
 
 app.put('/api/v1/whisper/:id', async (req, res) => {
-    await updateById(parseInt(req.params.id), req.body.message)
-    res.sendStatus(200)
+  await updateById(parseInt(req.params.id), req.body.message)
+  res.sendStatus(200)
 })
 
 app.delete('/api/v1/whisper/:id', async (req, res) => {
-    await deleteById(parseInt(req.params.id))
-    res.sendStatus(200)
+  await deleteById(parseInt(req.params.id))
+  res.sendStatus(200)
 })
 
 app.get('/', async (req, res) => {
-    res.render('index', { title: 'Home Page', whispers: await getAll() });
-});
+  res.render('index', { title: 'Home Page', whispers: await getAll() })
+})
 
 app.get('/about', async (req, res) => {
-    const whisperCount = (await getAll()).length;
-    res.render('about', { title: 'About Page', whisperCount });
-});
+  const whisperCount = (await getAll()).length
+  res.render('about', { title: 'About Page', whisperCount })
+})
 
 export { app }
--- tests/store.test.js
+++ tests/store.test.js
@@ -1,87 +1,86 @@
-import { getAll, getById, create, updateById, deleteById } from '../store.js';
+import { getAll, getById, create, updateById, deleteById } from '../store.js'
 import { writeFileSync } from 'node:fs'
 import { join } from 'node:path'
 
 const dbPath = join(process.cwd(), 'db.json')
-const restoreDb = () => writeFileSync(dbPath, JSON.stringify([]));
-const populateDb = (data) => writeFileSync(dbPath, JSON.stringify(data));
-const fixtures = [{ id: 1, message: 'Hello World' }, { id: 2, message: 'Hello Again' }];
-const inventedId = 999;
-const existingId = fixtures[0].id;
-
+const restoreDb = () => writeFileSync(dbPath, JSON.stringify([]))
+const populateDb = (data) => writeFileSync(dbPath, JSON.stringify(data))
+const fixtures = [{ id: 1, message: 'Hello World' }, { id: 2, message: 'Hello Again' }]
+const inventedId = 999
+const existingId = fixtures[0].id
 
 describe('store', () => {
-  beforeEach(() => populateDb(fixtures));
-  afterAll(() => restoreDb());
+  beforeEach(() => populateDb(fixtures))
+  afterAll(() => restoreDb())
 
   describe('getById', () => {
-      it('should return empty array if no data', async () => {
-      restoreDb();
-      const data = await getAll();
-      expect(data).toEqual([]);
-    });
+    it('should return empty array if no data', async () => {
+      restoreDb()
+      const data = await getAll()
+      expect(data).toEqual([])
+    })
 
     it('should return all data', async () => {
-      const data = await getAll();
-      expect(data).toEqual(fixtures);
-    });
-  });
+      const data = await getAll()
+      expect(data).toEqual(fixtures)
+    })
+  })
 
   describe('getById', () => {
     it('should return undefined if id does not exist', async () => {
-      const data = await getById(inventedId);
-      expect(data).toBeUndefined();
-    });
+      const data = await getById(inventedId)
+      expect(data).toBeUndefined()
+    })
     it('should return correct item if id exists', async () => {
-      const data = await getById(existingId);
-      expect(data).toEqual(fixtures[0]);
-    });
-  });
+      const data = await getById(existingId)
+      expect(data).toEqual(fixtures[0])
+    })
+  })
 
   describe('create', () => {
     it('should create new item', async () => {
-      const message = 'New Message';
-      const newItem = await create(message);
-      expect(newItem).toEqual({ id: 3, message });
-    });
-    it('should add item to the db', async() => {
-      const newItem = { id: fixtures.length + 1, message: 'test-3'}
-      const { id } = await create(newItem.message);
-      const item = await getById(id);
-      expect(item).toEqual(newItem);
-    });
-  });
+      const message = 'New Message'
+      const newItem = await create(message)
+      expect(newItem).toEqual({ id: 3, message })
+    })
+    it('should add item to the db', async () => {
+      const newItem = { id: fixtures.length + 1, message: 'test-3' }
+      const { id } = await create(newItem.message)
+      const item = await getById(id)
+      expect(item).toEqual(newItem)
+    })
+  })
 
   describe('updateById', () => {
     it('should return undefined if id does not exist', async () => {
-      const result = await updateById(inventedId, 'Updated Message');
-      expect(result).toBeUndefined();
-    });
+      const result = await updateById(inventedId, 'Updated Message')
+      expect(result).toBeUndefined()
+    })
     it('should not return item directly', async () => {
-      const result = await updateById(existingId, 'Updated Message');
-      expect(result).toBeUndefined();
-    });
+      const result = await updateById(existingId, 'Updated Message')
+      expect(result).toBeUndefined()
+    })
     it('should update item in the db', async () => {
-      const newMessage = 'Updated Message';
-      await updateById(existingId, newMessage);
-      const updatedItem = await getById(existingId);
-      expect(updatedItem).toEqual({ id: existingId, message: newMessage });
-    });
-  });
+      const newMessage = 'Updated Message'
+      await updateById(existingId, newMessage)
+      const updatedItem = await getById(existingId)
+      expect(updatedItem).toEqual({ id: existingId, message: newMessage })
+    })
+  })
 
   describe('deleteById', () => {
     it('should return undefined if id does not exist', async () => {
-      const result = await deleteById(inventedId);
-      expect(result).toBeUndefined();
-    });
+      const result = await deleteById(inventedId)
+      expect(result).toBeUndefined()
+    })
     it('should not return item directly', async () => {
-      const result = await deleteById(existingId);
-      expect(result).toBeUndefined();
-    });
+      const result = await deleteById(existingId)
+      expect(result).toBeUndefined()
+    })
     it('should delete item from the db', async () => {
-      await deleteById(existingId);
-      const items = await getAll();
-      expect(items).toEqual(fixtures.filter(item => item.id !== existingId));
-    });
-  });
-});
+      await deleteById(existingId)
+      const items = await getAll()
+      expect(items).toEqual(fixtures.filter(item => item.id !== existingId))
+    })
+  })
+})
